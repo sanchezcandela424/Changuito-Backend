99ef41c56a028185392b595438704f863b03fed9
Cande Sanchez <sanchezcandela424@gmail.com>
2025-06-11 20:21:38 -0300

    Order funcional se crea

--- src/main/java/com/example/buensabor/controller/OrderController.java
+++ src/main/java/com/example/buensabor/controller/OrderController.java
@@ -7,8 +7,29 @@ import com.example.buensabor.Bases.BaseControllerImplementation;
 import com.example.buensabor.entity.dto.OrderDTO;
 import com.example.buensabor.service.OrderService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping(path = "api/v1/order")
 public class OrderController extends BaseControllerImplementation<OrderDTO, OrderService>{
+    
+    @Autowired  
+    private OrderService orderService;
+
+    @PostMapping("")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT')")
+    @Override
+    public ResponseEntity<?> save(@RequestBody OrderDTO orderDTO) {
+        try {
+            return ResponseEntity.ok().body(orderService.save(orderDTO));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error to create the order: " + e.getMessage());
+        }        
+    }
     
 }
--- src/main/java/com/example/buensabor/service/OrderService.java
+++ src/main/java/com/example/buensabor/service/OrderService.java
@@ -2,47 +2,100 @@ package com.example.buensabor.service;
 
 
 import java.util.Date;
+import java.util.List;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import com.example.buensabor.Auth.CustomUserDetails;
 import com.example.buensabor.Bases.BaseServiceImplementation;
+import com.example.buensabor.entity.Client;
+import com.example.buensabor.entity.Company;
 import com.example.buensabor.entity.Order;
+import com.example.buensabor.entity.OrderProduct;
+import com.example.buensabor.entity.Product;
 import com.example.buensabor.entity.dto.OrderDTO;
+import com.example.buensabor.entity.dto.OrderProductDTO;
+import com.example.buensabor.entity.dto.ProductDTO;
 import com.example.buensabor.entity.enums.OrderStatus;
 import com.example.buensabor.entity.mappers.OrderMapper;
+import com.example.buensabor.repository.ClientRepository;
+import com.example.buensabor.repository.CompanyRepository;
+import com.example.buensabor.repository.OrderProductRepository;
 import com.example.buensabor.repository.OrderRepository;
+import com.example.buensabor.repository.ProductRepository;
 import com.example.buensabor.service.interfaces.IOrderService;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class OrderService extends BaseServiceImplementation<OrderDTO, Order, Long> implements IOrderService {
     
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final CompanyRepository companyRepository;
+    private final ClientRepository clientRepository;
+    private final ProductRepository productRepository;
+    private final OrderProductRepository orderProductRepository;
 
-    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper) {
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, CompanyRepository companyRepository, ClientRepository clientRepository, ProductRepository productRepository, OrderProductRepository orderProductRepository) {
         super(orderRepository, orderMapper);
         this.orderRepository = orderRepository;
         this.orderMapper = orderMapper;
+        this.companyRepository = companyRepository;
+        this.productRepository = productRepository;
+        this.orderProductRepository = orderProductRepository;
+        this.clientRepository = clientRepository;
     }
 
     @Override
+    @Transactional
     public OrderDTO save(OrderDTO orderDTO) {
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        
+        // Verificar que la Company existe
+        Client client = clientRepository.findById(userDetails.getId())
+        .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        List<OrderProductDTO> orderProductDTO = orderDTO.getOrderProductDTOs();
+
+        if (orderProductDTO.isEmpty()) {
+            throw new RuntimeException("There aren't products in the order");
+        }
+
         // Convertir DTO a entidad
         Order order = orderMapper.toEntity(orderDTO);
+        order.setClient(client);
+        order.setInitAt(new java.util.Date());
+        order.setStatus(OrderStatus.TOCONFIRM);
+        
+        Company company = companyRepository.findById(orderProductDTO.get(0).getProduct().getCompany().getId())
+            .orElseThrow(() -> new RuntimeException("Company not found"));
+        
+        order.setCompany(company);
+        
 
-        // Setear initAt si viene null
-        if (order.getInitAt() == null) {
-            order.setInitAt(new Date());
+        double total = 0;
+        // Validar y asociar los productos al OrderProduct
+        for (OrderProductDTO orderProductDTO2 : orderProductDTO) {
+            
+            Product product = productRepository.findById(orderProductDTO2.getProduct().getId())
+                .orElseThrow(() -> new RuntimeException("Product not found: " + orderProductDTO2.getProduct().getId()));
+            
+            OrderProduct orderProduct = new OrderProduct();
+            orderProduct.setOrder(order);
+            orderProduct.setProduct(product);
+            orderProduct.setQuantity(orderProductDTO2.getQuantity());
+            orderProduct.setPrice(product.getPrice() * orderProductDTO2.getQuantity());
+            total = total + orderProduct.getPrice();
+            
+            orderProductRepository.save(orderProduct);
         }
-
-        // Setear status inicial si viene null
-        if (order.getStatus() == null) {
-            order.setStatus(OrderStatus.TOCONFIRM);
-        }
-
-        // Guardar la entidad
+        
+        order.setTotal(total);
         Order savedOrder = orderRepository.save(order);
-
-        // Convertir de nuevo a DTO para devolver
+        // Retornar DTO
         return orderMapper.toDTO(savedOrder);
     }
+
 }
