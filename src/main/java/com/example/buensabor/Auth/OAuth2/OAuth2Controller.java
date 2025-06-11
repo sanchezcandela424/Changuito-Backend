a45d1f18fac051983d68b20f2eb77831e8018340
Cande Sanchez <sanchezcandela424@gmail.com>
2025-06-11 13:03:16 -0300

    corregido el url y la redireccion

--- src/main/java/com/example/buensabor/Auth/OAuth2/OAuth2Controller.java
+++ src/main/java/com/example/buensabor/Auth/OAuth2/OAuth2Controller.java
@@ -2,12 +2,12 @@ package com.example.buensabor.Auth.OAuth2;
 
 import java.io.IOException;
 
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -17,12 +17,12 @@ public class OAuth2Controller {
     @GetMapping("/google-login")
     public void loginWithGoogle(@RequestParam(required = false) String redirect, HttpServletResponse response) throws IOException {
         if (redirect != null) {
-            ResponseCookie cookie = ResponseCookie.from("redirect_url", redirect)
-                .path("/")
-                .httpOnly(false)
-                .maxAge(300) // 5 minutos
-                .build();
-            response.addHeader("Set-Cookie", cookie.toString());
+            Cookie cookie = new Cookie("redirect_url", redirect);
+            cookie.setPath("/");
+            cookie.setHttpOnly(true); // Podés poner true si querés más seguridad
+            cookie.setMaxAge(300); // 5 minutos
+            // cookie.setSecure(true); // solo si estás en HTTPS
+            response.addCookie(cookie);
         }
         response.sendRedirect("/oauth2/authorization/google");
     }
